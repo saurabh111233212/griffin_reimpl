0ada0fd6c22cc7f3605586680cb38b059677cefb
Saurabh (Siri) Shah <saurabh_shah2@saurabh-shah2-base.siri-interactive-vm.svc.kube.us-west-3b.k8s.cloud.apple.com>
2024-07-12 00:01:45 +0000

    RMSNorm

--- griffin/griffin.py
+++ griffin/griffin.py
@@ -5,3 +5,38 @@ from .scan import scan
 
 from flash_attn import flash_attn_func
 from flash_attn.layers.rotary import RotaryEmbedding
+
+
+class RMSNorm(nn.Module):
+    def __init__(self, dim, epsilon=1e-8, bias=False):
+        super().__init__()
+        
+        self.dim = dim
+        self.epsilon = epsilon
+        self.bias = bias
+        
+        self.weight = nn.Parameter(torch.ones(dim))
+        if bias:
+            self.bias = nn.Parameter(torch.zeros(dim))
+        else:
+            self.bias = 0
+    
+    def forward(self, x):
+        # x.shape = (batch, dim)
+        rms = torch.sqrt(torch.mean(x ** 2, dim=-1, keepdim=True) + self.epsilon)
+        x_normed = x / rms
+        self.weight * x_normed + self.bias
+        
+                
+
+class GatedMLP(nn.Module):
+    pass
+
+class RecurrentBlock(nn.Module):
+    pass
+
+class ResidualBlock(nn.Module):
+    pass
+
+class Griffin(nn.Module):
+    pass
